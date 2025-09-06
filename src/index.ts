f0767878062adc1e0cb5626906d795064021bd93
Ville Seppanen <ville.t.seppanen@iki.fi>
2025-09-06 13:13:06 +0300

    Update prompt

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,27 @@ export const mainApp = async (dryrun: boolean): Promise<void> => {
   const openAiClient: OpenAiClient = new OpenAiClient();
   const telegramClient: TelegramClient = new TelegramClient();
 
-  const prompt = "Anna yhteenveto päivän merkittävimmistä uutisista suomeksi.";
+  const prompt = `
+    Anna yhteenveto päivän merkittävimmistä uutisista suomeksi.
+    Anna pelkät uutiset, älä tervehdi tai kirjoita alkuun kuvausta tai loppuun mitään jatkokysymystä.
+    Älä käytä ranskalaisia viivoja, vaan kirjoita uutiset erillisinä riveinä.
+    Tee jokaisen uutisen väliin tyhjä rivi.
+    Korkeintaan 10 uutista.
+    Älä lisää mitään otsikoita tai ryhmittelyitä, äläkä käytä emojeita.`;
   const message = (await openAiClient.getResponse(prompt)).output_text;
-  console.log(message);
 
   if (!dryrun) {
     await telegramClient.sendMessage(message);
   }
   else {
     console.log("Dryrun, not sending a message to Telegram");
+    console.log(message);
   }
 };
 
 
 export const handler = async (event: LambdaEvent): Promise<LambdaResponse> => {
-  console.log(JSON.stringify(event));
-  const dryrunMessageSending = event.source == 'local-dryrun' ? true : false;
+  const dryrunMessageSending = event.source == 'dryrun' ? true : false;
   await mainApp(dryrunMessageSending);
   return {
     statusCode: 200,
@@ -30,7 +35,7 @@ export const handler = async (event: LambdaEvent): Promise<LambdaResponse> => {
 };
 
 if (require.main == module) {
-  void handler({source: 'local-dryrun'});
+  void handler({source: 'dryrun'});
 }
 
 export interface LambdaEvent {
